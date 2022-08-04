5e2506cd8bb3c172b2426dbfd3cf9777ffe9634f
wd-francois <73915424+wd-francois@users.noreply.github.com>
2022-08-05 07:06:43 +1000

    update about page

--- pages/about.js
+++ pages/about.js
@@ -1,19 +1,79 @@
 import Layout from '@/components/Layout'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function AboutPage() {
   return (
-    <Layout title='About DevSpace'>
-      <h1 className='text-5xl border-b-4 pb-5 font-bold'>About</h1>
-
-      <div className='bg-white shadow-md rounded-lg px-10 py-6 mt-6'>
-        <h3 className='text-2xl mb-5'>DevSpace Blog</h3>
-
-        <p className='mb-3'>This is a blog built with Next.js and Markdown</p>
-
-        <p>
-          <span className='font-bold'>Version 1.0.0</span>
+    <Layout title='About Fusion Strength & Conditioning'>
+       
+       <div className="mt-32">
+       <div className="flex flex-wrap justify-center md:mt-8">
+        <div className="max-w-full h-auto rounded align-middle border-none">
+        <Image src='/images/profile1.jpg' width={600} height={400} alt="personal trainer profile"/>
+          </div>
+          </div>
+               <h5 className='font-bold text-2xl tracking-tight mb-2'>About</h5>
+                <p>Fusion Strength and Conditioning is a fully equipped personal training studio and gym in Melbourne’s North. 
+            As a home fitness studio and gym, clients can train in a comfortable space, without the distractions of a commercial gym.
+            All training programs are individually tailored, with a focus on achieving goals in a safe and supportive environment.</p>
+          
+            <p>Francois Swart is the trainer behind Fusion Strength and Conditioning. Francois is a nationally certified personal trainer
+            through Fitness Australia, with additional qualifications in Exercise Rehabilitation, Strength Training and Nutrition.</p>
+        
+            <p>With more than 15 years of experience, he has worked with a wide range of clients with varying goals and abilities – from athletes
+            who want to improve at their sport, to everyday people who want to lose some weight, build strength or improve their overall health and wellbeing.</p>
+        
+            <p>While much of his career has been spent in commercial gyms, the challenges of 2020 also presented new opportunity. Francois has now started his own
+            personal training studio and gym at his home in Melbourne’s North.</p>
+        
+            <p>
+            Francois is passionate about helping people achieve their goals, and is focused on the individual needs of each client.
+            He believes that everyone is different, so each training program should reflect this. There is no ‘one size fits all’ model.
+            His training programs are built on simple, safe and proven principles, but his methods are always changing and evolving so his clients can progressively,
+            and safely, build toward their ultimate goal. He is committed to supporting his clients outside of their training sessions too, from providing at home programs
+            to advice on healthy eating and nutrition.</p>
+            <p>Get in touch today to start achieving your health and fitness goals!</p>
+       </div>
+    <div>
+    </div>
+       <footer className="bg-white">
+    <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
+        <nav className="flex flex-wrap justify-center -mx-5 -my-2">
+                 <div className="px-5 py-2"></div>
+                 <div className="px-5 py-2">
+            <Link href='/blog'className="text-base leading-6 text-gray-500 hover:text-gray-900">
+                    Blog
+                </Link>
+            </div>
+            <div className="px-5 py-2">
+            <Link href='/services'className="text-base leading-6 text-gray-500 hover:text-gray-900">
+                    Services
+                </Link>
+            </div>
+            <div className="px-5 py-2">
+            <Link href='/about'className="text-base leading-6 text-gray-500 hover:text-gray-900">
+                    About
+                </Link>
+            </div>
+            <div className="px-5 py-2">
+            <Link href='/contact'className="text-base leading-6 text-gray-500 hover:text-gray-900">
+                    Contact
+                </Link>
+            </div>
+                  </nav>
+        <div className="flex justify-center mt-8 space-x-6">
+                     <a href="https://www.instagram.com/francois.pt/?hl=en" className="text-gray-400 hover:text-gray-500">
+                <span className="sr-only">Instagram</span>
+                <svg className="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"></path>
+                </svg>
+            </a>
+        </div>
+        <p className="mt-8 text-base leading-6 text-center text-gray-400">
+            © 2022 F-Web-Dev, Inc. All rights reserved.
         </p>
-      </div>
+    </div>
+  </footer>
     </Layout>
   )
 }
